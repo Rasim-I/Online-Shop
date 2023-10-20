f595295ba92d415ae6902a0d144a76cf54adfb92
Rasim-I <rasimismailov6@gmail.com>
2023-10-20 22:10:35 +0300

    main page grid

---
+++ Online Shop/Views/Home/Partial/_RecentItemsPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Online_Shop.Views.Home.Partial;
+
+public class _RecentItemsPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Online Shop/Views/Home/Partial/_RecommendationItemPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Online_Shop.Views.Home.Partial;
+
+public class _RecommendationItemPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
