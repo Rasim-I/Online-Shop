4188c9bbcc4236aea8b6aa15afc560c3c48b0e06
Rasim-I <rasimismailov6@gmail.com>
2023-10-30 20:07:41 +0200

    catalog partial update

---
+++ Online Shop/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers;
+
+[Controller]
+public class ItemController : Controller
+{
+    private readonly ILogger<ItemController> _logger;
+
+    public ItemController(ILogger<ItemController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult ItemCategory([FromQuery] string categoryId)
+    {
+        return View();
+    }
+}
