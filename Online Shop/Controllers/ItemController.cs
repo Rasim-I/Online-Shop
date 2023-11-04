42245ddc41476b040e1cea8ebb2a7d60e8fbf9d7
Rasim-I <rasimismailov6@gmail.com>
2023-11-04 17:54:34 +0200

    ItemCategory update
    clickable item, item rating images

--- Online Shop/Controllers/ItemController.cs
+++ Online Shop/Controllers/ItemController.cs
@@ -64,4 +64,10 @@ public class ItemController : Controller
         ViewBag.Category = category;
         return View(items);
     }
+
+    public string ItemPage([FromQuery] string itemId)
+    {
+        return itemId;
+    }
+    
 }
