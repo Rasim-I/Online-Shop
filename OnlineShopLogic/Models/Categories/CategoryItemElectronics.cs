16af7ac982921a8d8231c4c51a41b696b37cb510
Rasim-I <rasimismailov6@gmail.com>
2023-12-10 14:50:51 +0200

    CategoryItemTypes

---
+++ OnlineShopLogic/Models/Categories/CategoryItemElectronics.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineShopLogic.Models.Categories;
+
+public class CategoryItemElectronics : Category
+{
+    private string _cpuModel;
+    private int _memoryCapacity;
+
+    public string CpuModel
+    {
+        get => _cpuModel;
+        set => value = _cpuModel;
+    }
+
+    public int MemoryCapacity
+    {
+        get => _memoryCapacity;
+        set => value = _memoryCapacity;
+    }
+    
+}
---
+++ OnlineShopLogic/Models/Categories/CategoryItemSport.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShopLogic.Models.Categories;
+
+public class CategoryItemSport : Category
+{
+   private string _category;
+
+   public string Category
+   {
+      get => _category;
+      set => value = _category;
+   }
+   
+   
+}
